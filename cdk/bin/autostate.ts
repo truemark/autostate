0492a0c21152190411a1c3a0558ff8fdc7435295
Lisa Koivu <lisamarie.koivu@gmail.com>
2024-02-14 11:14:25 -0500

    split cdk and handlers

--- cdk/bin/autostate.ts
+++ cdk/bin/autostate.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AutoStateStack } from '../lib/autostate-stack';
+import {AutoStateStack} from '../lib/autostate-stack';
 
 const app = new cdk.App();
 new AutoStateStack(app, 'AutoState', {});
---
+++ cdk/lib/scheduler-function.ts
@@ -0,0 +1,43 @@
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
+import {Construct} from 'constructs';
+import {Architecture, Runtime} from 'aws-cdk-lib/aws-lambda';
+import {Duration} from 'aws-cdk-lib';
+import {RetentionDays} from 'aws-cdk-lib/aws-logs';
+import * as path from 'path';
+import {PolicyStatement} from 'aws-cdk-lib/aws-iam';
+
+interface SchedulerFunctionProps {
+  readonly tagPrefix: string;
+}
+
+export class SchedulerFunction extends NodejsFunction {
+  constructor(scope: Construct, id: string, props: SchedulerFunctionProps) {
+    super(scope, id, {
+      runtime: Runtime.NODEJS_18_X,
+      architecture: Architecture.ARM_64,
+      memorySize: 512,
+      timeout: Duration.seconds(300),
+      logRetention: RetentionDays.ONE_MONTH,
+      entry: path.join(__dirname, '..', '..', 'handlers', 'src', 'scheduler.ts'),
+      handler: 'handler',
+      environment: {
+        TAG_PREFIX: props.tagPrefix,
+      },
+    });
+
+    this.addToRolePolicy(
+      new PolicyStatement({
+        actions: [
+          'ec2:DescribeInstances',
+          'rds:DescribeDBClusters',
+          'rds:DescribeDBInstances',
+          'rds:DescribeEvents',
+          'ecs:DescribeServices',
+          'ecs:ListTagsForResource',
+          'states:StartExecution',
+        ],
+        resources: ['*'],
+      })
+    );
+  }
+}
