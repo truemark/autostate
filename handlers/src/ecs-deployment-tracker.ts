baa9e8af60701fcf1ebcf3e81f6a5d00380b1fb2
Taurai Nyamakura <tnyamakura@Taurais-MBP-2.home>
2024-05-20 10:11:37 +0200

    Reduce the amount of logging

--- handlers/src/ecs-deployment-tracker.ts
+++ handlers/src/ecs-deployment-tracker.ts
@@ -4,7 +4,6 @@ const ecsClient = new ECSClient({});
 const ECS_REQUIRED_TAG_KEY = 'autostate:max-runtime';
 
 export async function applyLatestTag(serviceArn: string): Promise<boolean> {
-  console.info(`Processing ECS service tags for service: ${serviceArn}`);
   try {
     const tagData = await ecsClient.send(new ListTagsForResourceCommand({ resourceArn: serviceArn }));
     const tags = tagData.tags;
@@ -12,7 +11,6 @@ export async function applyLatestTag(serviceArn: string): Promise<boolean> {
     const hasRequiredTag = tags.some(tag => tag.key === ECS_REQUIRED_TAG_KEY);
 
     if (!hasRequiredTag) {
-      console.info('Service does not have the required tag. Exiting without action.');
       return false;
     }
 
@@ -24,7 +22,6 @@ export async function applyLatestTag(serviceArn: string): Promise<boolean> {
       resourceArn: serviceArn,
       tagKeys: [ECS_REQUIRED_TAG_KEY]
     }));
-    console.log('Tag successfully cleared.');
 
     await ecsClient.send(new TagResourceCommand({
       resourceArn: serviceArn,
@@ -38,14 +35,11 @@ export async function applyLatestTag(serviceArn: string): Promise<boolean> {
     return true;
 
   } catch (error) {
-    console.error('Error processing ECS service tags:', error);
     throw new Error(`Error processing ECS service tags: ${error}`);
   }
 }
 
 export async function handler(event: any): Promise<any> {
-  console.info('Received event:', JSON.stringify(event));
-
   const serviceArnList: string[] = event.resources;
 
   for (const serviceArn of serviceArnList) {
