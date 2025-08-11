cdcfa7236b736d0f6b1c0d9c0bff896abc6994e6
Fouad Roumieh <froumieh@truemark.io>
2025-08-11 15:19:04 +0200

    Fixed styling for prettier

--- cdk/lib/scheduler-function.ts
+++ cdk/lib/scheduler-function.ts
@@ -36,8 +36,8 @@ export class SchedulerFunction extends NodejsFunction {
       new PolicyStatement({
         actions: [
           'ec2:DescribeInstances',
-          "sagemaker:DescribeNotebookInstance",
-          "sagemaker:ListTags",
+          'sagemaker:DescribeNotebookInstance',
+          'sagemaker:ListTags',
           'rds:DescribeDBClusters',
           'rds:DescribeDBInstances',
           'rds:DescribeEvents',
